a7eda695c1071f6f322ce28fd0bcd717ea8cf8fc
medPurple <medpurple.pro@gmail.com>
2025-03-21 14:28:35 +0100

    Ajout de librairie faker et ajout du script de generation

---
+++ generate_user.js
@@ -0,0 +1,62 @@
+import mysql from 'mysql2/promise';
+import { faker } from '@faker-js/faker';
+import { v4 as uuidv4 } from 'uuid';
+
+// Configuration MySQL
+const dbConfig = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Unmotdep@sse32mysql',
+    database: 'pappers_local',
+};
+
+async function connectToDB() {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        console.log('Connecté à la base de données');
+        return connection;
+    } catch (error) {
+        console.error('Erreur de connexion:', error);
+        process.exit(1);
+    }
+}
+
+function generateUser() {
+    return [
+        uuidv4(),
+        faker.internet.email(),
+        faker.internet.password(12),
+        faker.person.lastName(),
+        faker.person.firstName(),
+        faker.internet.username(),
+        faker.number.int({ min: 18, max: 60 }),
+        faker.helpers.arrayElement(['F', 'M', 'O']),
+        faker.helpers.arrayElement(['F', 'M', 'O']),
+    ];
+}
+
+
+async function insertFakeUsers(n) {
+    const connection = await connectToDB();
+
+    const sql = `
+        INSERT INTO utilisateurs (uuid, email, mot_de_passe, nom, prenom, pseudo, age, genre, orientation, profil_complet)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, 1)
+    `;
+
+    const users = Array.from({ length: n }, generateUser);
+
+    try {
+        for (const user of users) {
+            await connection.execute(sql, user);
+        }
+        console.log(`${n} utilisateurs fictifs ajoutés avec succès`);
+    } catch (error) {
+        console.error('Erreur lors de l’insertion:', error);
+    } finally {
+        await connection.end();
+    }
+}
+
+insertFakeUsers(10);
